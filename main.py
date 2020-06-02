9250d3ff343da4631b796488526f3fd82aca12bb
Salvador Guzman <guzmansalv@gmail.com>
2020-06-02 08:36:05 -0700

    code compiles

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ def assert_cmd(args: List[str]) -> None:
 def get_csv(args: List[str]) -> str:
     return args[1]
 
+def format(idd: str, md5: str, author: str, title: str, publisher: str, year: str, extension: str) -> str:
+    return ''
+
+
 def main() -> None:
     assert_cmd(sys.argv)
     csv_file: str = sys.argv[1]
-    
+
     with open(csv_file) as csv_fd:
         print('Opened file', csv_file, 'succesfully')
         csv_read = csv.reader(csv_fd, delimiter=',', quotechar='"')
         for row in csv_read:
             print(row)
-    print('Hello')
 
 if __name__ == '__main__':
     main()
